6d6ddbea7933234fcf993f7489bffd2fd55f6760
prerna635 <prernagupta635@Gmail.com>
2025-07-28 22:13:43 +0530

    Add files via upload

---
+++ Task2_GradeCalculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task2_GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for Subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+            if (marks[i] < 0 || marks[i] > 100) {
+                System.out.println("Invalid input! Marks should be between 0 and 100.");
+                i--;
+                continue;
+            }
+            totalMarks += marks[i];
+        }
+        double averagePercentage = (double) totalMarks / numSubjects;
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else if (averagePercentage >= 50) {
+            grade = 'E';
+        } else {
+            grade = 'F';
+        }
+        System.out.println();
+        System.out.println("Total Marks Obtained: " + totalMarks + " out of " + (numSubjects * 100));
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+        scanner.close();
+    }
+}
